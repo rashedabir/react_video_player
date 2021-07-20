6e2a70e6787b5c7c515865dba89389b170ebda0c
rashedabir <rashed15-2155@diu.edu.bd>
2021-07-20 18:36:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Grid,
+  Typography,
+  makeStyles,
+  Container,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import "./App.css";
+import { videoData } from "./utils/videoData";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    padding: "50px 0",
+  },
+  songList: {
+    backgroundColor: "white",
+    maxHeight: "47.5vh",
+    overflowX: "auto",
+  },
+  title: {
+    margin: "15px 0",
+    color: "white",
+  },
+  heading: {
+    textAlign: "center",
+    color: "white",
+    paddingTop: "50px",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [link, setLink] = useState("");
+  const [title, setTitle] = useState("");
+
+  const handleListItemClick = (event, index, src, name) => {
+    setSelectedIndex(index);
+    setLink(src);
+    setTitle(name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h4" className={classes.heading}>
+        React Video Player
+      </Typography>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={6}>
+            <ReactPlayer url={link} width="100%" controls playing />
+            {selectedIndex === 0 ? (
+              <Typography variant="h5" className={classes.title}>
+                Select a Song from Playlist
+              </Typography>
+            ) : (
+              <Typography variant="h5" className={classes.title}>
+                <i className="far fa-play-circle"></i> Now Playing:{" "}
+                <strong>
+                  {selectedIndex}. {title}
+                </strong>
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <List
+              aria-label="main mailbox folders"
+              className={classes.songList}
+            >
+              {videoData.map((item) => (
+                <ListItem
+                  button
+                  selected={selectedIndex === item.id}
+                  onClick={(event) =>
+                    handleListItemClick(event, item.id, item.src, item.name)
+                  }
+                >
+                  <ListItemIcon>
+                    <PlayCircleOutlineIcon />
+                  </ListItemIcon>
+                  <ListItemText>
+                    {" "}
+                    {item.id}. {item.name}{" "}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/utils/videoData.jsx
@@ -0,0 +1,77 @@
+export const videoData = [
+  {
+    id: 1,
+    name: "Tera Woh Pyar",
+    src: "https://youtu.be/y45lh8oBzOo",
+  },
+  {
+    id: 2,
+    name: "How To Make A Responsive Video Gallery Using Javascript",
+    src: "https://youtu.be/ph0TZy6LmGQ",
+  },
+  {
+    id: 3,
+    name: "CHICKEN FRY | FRIED CHICKEN Recipe",
+    src: "https://youtu.be/YiXLGFN2pc0",
+  },
+  {
+    id: 4,
+    name: "Charlie Puth - One Call Away [Official Video]",
+    src: "https://youtu.be/BxuY9FET9Y4",
+  },
+  {
+    id: 5,
+    name: "Charlie Puth - How Long [Official Video]",
+    src: "https://youtu.be/CwfoyVa980U",
+  },
+  {
+    id: 6,
+    name: "Sia - Cheap Thrills (Lyric Video) ft. Sean Paul",
+    src: "https://youtu.be/nYh-n7EOtMA",
+  },
+  {
+    id: 7,
+    name: "Aabaad Barbaad (Full VIdeo)",
+    src: "https://youtu.be/jh66Pjtqr4k",
+  },
+  {
+    id: 8,
+    name: "Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)",
+    src: "https://youtu.be/aJOTlE1K90k",
+  },
+  {
+    id: 9,
+    name: "Powfu - death bed (coffee for your head) (Official Video) ft. beabadoobee",
+    src: "https://youtu.be/jJPMnTXl63E",
+  },
+  {
+    id: 10,
+    name: "Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez) [Official Video]",
+    src: "https://youtu.be/3AtDnEC4zak",
+  },
+  {
+    id: 11,
+    name: "Christina Perri - A Thousand Years [Official Music Video]",
+    src: "https://youtu.be/rtOvBOTyX00",
+  },
+  {
+    id: 12,
+    name: "Alan Walker - Faded",
+    src: "https://youtu.be/60ItHLz5WEA",
+  },
+  {
+    id: 13,
+    name: "Alan Walker, Sabrina Carpenter & Farruko - On My Way",
+    src: "https://youtu.be/dhYOPzcsbGM",
+  },
+  {
+    id: 14,
+    name: "Maroon 5 - Memories (Official Video)",
+    src: "https://youtu.be/SlPhMPnQ58k",
+  },
+  {
+    id: 15,
+    name: "Otilia - Bilionera (official video)",
+    src: "https://youtu.be/-j0dlcfekqw",
+  },
+];
